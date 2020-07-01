abd931457e009ecaaa7e058347d2492672942c3e
Msoliman512 <mesaam17@gmail.com>
2020-07-01 12:01:11 +0200

    Add Utils and nullity check to products that been passed to AddRating method

---
+++ MyFirstWebApp.WebSite/Utils/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApp.WebSite.Utills
+{
+    public static class Utility
+    {
+        public static bool IsAnyProduct<T>(this IEnumerable<T> data)
+        {
+            return data != null && data.Any();
+        }
+    }
+}
